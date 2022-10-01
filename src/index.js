e2add1d2975ff858d33a534189856b5e3221b5aa
MartinezFabian <FabianMartinezDev@gmail.com>
2022-10-01 19:54:26 -0300

    Se agrega la interfaz de login con sus estilos

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const createWindow = () => {
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile("src/index.html");
+  mainWindow.loadFile("src/login.html");
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
