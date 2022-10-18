78e20ca7fe9cb4499f18882a22188c0f0a0eb50b
Rodrigo Saforcada <ronninn18@gmail.com>
2022-10-18 14:57:38 -0300

    Sistema de roles completado

--- src/js/restringirPorRol.js
+++ src/js/restringirPorRol.js
@@ -6,3 +6,19 @@ if(sessionStorage.getItem('rolUsuario') == 1) {
     document.querySelector('#cargar_consumicion').style.display = 'none';
     document.querySelector('#registro').style.display = 'none';
 }
+if(sessionStorage.getItem('rolUsuario') == 2) {
+    document.querySelector('#pedidos_pendientes').style.display = 'none';
+    document.querySelector('#cargar_pedidos').style.display = 'none';
+    document.querySelector('#clientes').style.display = 'none';
+    document.querySelector('#ingresos').style.display = 'none';
+    document.querySelector('#cargar_consumicion').style.display = 'none';
+    document.querySelector('#registro').style.display = 'none';
+}
+if(sessionStorage.getItem('rolUsuario') == 3) {
+    document.querySelector('#reservas').style.display = 'none';
+    document.querySelector('#cargar_pedidos').style.display = 'none';
+    document.querySelector('#clientes').style.display = 'none';
+    document.querySelector('#ingresos').style.display = 'none';
+    document.querySelector('#cargar_consumicion').style.display = 'none';
+    document.querySelector('#registro').style.display = 'none';
+}
