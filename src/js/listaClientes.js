e3ad51bd54407dce08830a6947e08f8166999b67
MartinezFabian <FabianMartinezDev@gmail.com>
2022-10-19 15:13:05 -0300

    Se agregaron estilos a las listas de mesas reservadas y clientes

--- src/js/listaClientes.js
+++ src/js/listaClientes.js
@@ -1,21 +1,35 @@
-import UsuarioRegistradoService from './usuariosRegistradosService.js';
+import UsuarioRegistradoService from "./usuariosRegistradosService.js";
 
-const listadoClientes = document.getElementById('contenedor_listado_clientes');
+const listadoClientes = document.getElementById("contenedor_listado_clientes");
 
-const listaUsuarios = new UsuarioRegistradoService;
+const listaUsuarios = new UsuarioRegistradoService();
 
-listaUsuarios.getUsuarioRegistrado()
-    .then((usuarios) => {
-        let listaUsuarios = Object.keys(usuarios).map((nombre) => usuarios[nombre]);
-        console.log(listaUsuarios);
+listaUsuarios
+  .getUsuarioRegistrado()
+  .then((usuarios) => {
+    let listaUsuarios = Object.keys(usuarios).map((nombre) => usuarios[nombre]);
+    console.log(listaUsuarios);
 
-        listaUsuarios.forEach( cliente => {
-            if(cliente.rol == 'cliente') {
-                const clientesAgendados = document.createElement('h3');
-                clientesAgendados.innerText = `${cliente.nombre}`;
-                listadoClientes.appendChild(clientesAgendados);
-            }
-        })
-
-    })
-    .catch(error => console.log(error));
+    listaUsuarios.forEach((cliente) => {
+      if (cliente.rol == "cliente") {
+        const clientesAgendados = document.createElement("p");
+        clientesAgendados.innerText = `${cliente.nombre}`;
+        listadoClientes.appendChild(clientesAgendados);
+        clientesAgendados.style.marginLeft = "90px";
+        clientesAgendados.style.marginRight = "90px";
+        clientesAgendados.style.marginBottom = "0px";
+        clientesAgendados.style.marginTop = "0px";
+        clientesAgendados.style.padding = "0px";
+        const subrayado = document.createElement("p");
+        subrayado.style.marginTop = "0px";
+        subrayado.style.marginBottom = "0px";
+        subrayado.style.padding = "0px";
+        subrayado.style.color = "gray";
+        subrayado.innerText =
+          "....................................................................................................";
+        listadoClientes.appendChild(clientesAgendados);
+        listadoClientes.appendChild(subrayado);
+      }
+    });
+  })
+  .catch((error) => console.log(error));
--- src/js/mesasReservadas.js
+++ src/js/mesasReservadas.js
@@ -1,22 +1,29 @@
-import UsuarioRegistradoService from './usuariosRegistradosService.js';
+import UsuarioRegistradoService from "./usuariosRegistradosService.js";
 
-const listadoClientes = document.getElementById('contenedor_mesas_reservadas');
-listadoClientes.style.textAlign = 'center';
+const listadoClientes = document.getElementById("contenedor_mesas_reservadas");
+listadoClientes.style.textAlign = "center";
 
-const listaUsuarios = new UsuarioRegistradoService;
+const listaUsuarios = new UsuarioRegistradoService();
 
-listaUsuarios.getUsuarioRegistrado()
-    .then((usuarios) => {
-        let listaUsuarios = Object.keys(usuarios).map((nombre) => usuarios[nombre]);
-        console.log(listaUsuarios);
+listaUsuarios
+  .getUsuarioRegistrado()
+  .then((usuarios) => {
+    let listaUsuarios = Object.keys(usuarios).map((nombre) => usuarios[nombre]);
+    console.log(listaUsuarios);
 
-        listaUsuarios.forEach( cliente => {
-            if(cliente.reservasPagadas != undefined) {
-                const clientesAgendados = document.createElement('h3');
-                clientesAgendados.innerText = `${cliente.nombre} ha reservado los días ${cliente.reservasPagadas}`;
-                listadoClientes.appendChild(clientesAgendados);
-            }
-        })
-
-    })
-    .catch(error => console.log(error));
+    listaUsuarios.forEach((cliente) => {
+      if (cliente.reservasPagadas != undefined) {
+        const clientesAgendados = document.createElement("p");
+        clientesAgendados.innerText = `${cliente.nombre} ha reservado los días ${cliente.reservasPagadas}`;
+        clientesAgendados.style.marginLeft = "220px";
+        clientesAgendados.style.marginRight = "220px";
+        const subrayado = document.createElement("p");
+        subrayado.innerText =
+          "..............................................................................................................................................................................................................";
+        subrayado.style.color = "gray";
+        listadoClientes.appendChild(clientesAgendados);
+        listadoClientes.appendChild(subrayado);
+      }
+    });
+  })
+  .catch((error) => console.log(error));
